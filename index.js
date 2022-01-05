402b48779c336c4b4a3e403d87126f60f422861f
Eric Chin <ericychin94@gmail.com>
2022-01-05 13:50:45 -0800

    Added POST route

--- index.js
+++ index.js
@@ -52,6 +52,20 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 });
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    const newPerson = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    };
+
+    persons = persons.concat(newPerson);
+
+    response.json(newPerson);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
