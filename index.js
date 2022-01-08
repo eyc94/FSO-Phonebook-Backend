608e5d08e34b25848b65429dda5c310d052c97af
Eric Chin <ericychin94@gmail.com>
2022-01-07 20:31:17 -0800

    New build with fixed spelling error for static middleware

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('buid'));
+app.use(express.static('build'));
 app.use(cors());
 
 // Part 3-7.
