c214006ad83310961bef4bfe019c4a404836cf57
Eric Chin <ericychin94@gmail.com>
2022-01-07 17:28:57 -0800

    Imported cors middleware and used it

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Part 3-7.
 // app.use(morgan('tiny'));
