f7fc5f827fd39143e57318dcdf62d85f2c5e121d
Eric Chin <ericychin94@gmail.com>
2022-01-10 14:42:52 -0800

    Copied code from mongo.js to its own module

---
+++ models/person.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+    console.log(
+        'Please provide the password as an argument: node mongo.js <password> or node mongo.js <password> <name> <number>'
+    );
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://first-user:${password}@cluster0.yv4do.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+    console.log('Phonebook:');
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number);
+        });
+        mongoose.connection.close();
+    });
+} else if (process.argv.length < 6) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    });
+
+    person.save().then(result => {
+        console.log('person saved!');
+        mongoose.connection.close();
+    });
+}
