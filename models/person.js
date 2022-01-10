f79be3c459e7dcfdcf21597cedfe32a7f36e54c8
Eric Chin <ericychin94@gmail.com>
2022-01-10 15:16:51 -0800

    Removed comments in module

--- models/person.js
+++ models/person.js
@@ -26,23 +26,3 @@ personSchema.set('toJSON', {
 });
 
 module.exports = mongoose.model('Person', personSchema);
-
-// if (process.argv.length === 3) {
-//     console.log('Phonebook:');
-//     Person.find({}).then(result => {
-//         result.forEach(person => {
-//             console.log(person.name, person.number);
-//         });
-//         mongoose.connection.close();
-//     });
-// } else if (process.argv.length < 6) {
-//     const person = new Person({
-//         name: process.argv[3],
-//         number: process.argv[4],
-//     });
-
-//     person.save().then(result => {
-//         console.log('person saved!');
-//         mongoose.connection.close();
-//     });
-// }
