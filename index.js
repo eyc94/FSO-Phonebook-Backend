23da406064ae524a2ad19d22590ef0fffc0ebc62
Eric Chin <ericychin94@gmail.com>
2022-01-10 15:17:15 -0800

    Removed hard-coded commented persons variable

--- index.js
+++ index.js
@@ -20,29 +20,6 @@ morgan.token('payload', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :payload'));
 
-// let persons = [
-//     {
-//         id: 1,
-//         name: "Arto Hellas",
-//         number: "040-123456"
-//     },
-//     {
-//         id: 2,
-//         name: "Ada Lovelace",
-//         number: "39-44-5323532"
-//     },
-//     {
-//         id: 3,
-//         name: "Dan Abramov",
-//         number: "12-43-234345"
-//     },
-//     {
-//         id: 4,
-//         name: "Mary Poppendieck",
-//         number: "39-23-6423122"
-//     }
-// ];
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons);
