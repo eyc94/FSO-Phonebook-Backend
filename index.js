9acef0d81947bcb1c5fd3e610577d72d4c2fc448
Eric Chin <ericychin94@gmail.com>
2022-01-10 15:07:14 -0800

    Transform toJSON method

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ let persons = [
 ];
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then(persons => {
+        response.json(persons);
+    });
 });
 
 app.get('/info', (request, response) => {
--- models/person.js
+++ models/person.js
@@ -17,6 +17,14 @@ const personSchema = new mongoose.Schema({
     number: String
 });
 
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+
 module.exports = mongoose.model('Person', personSchema);
 
 // if (process.argv.length === 3) {
