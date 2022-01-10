2715304881f093bb0b4870f86a5c7b1bb10426cb
Eric Chin <ericychin94@gmail.com>
2022-01-10 15:04:13 -0800

    Added handlers to connection, exported module, and commented conditionals

--- models/person.js
+++ models/person.js
@@ -4,31 +4,37 @@ const url = process.env.MONGODB_URI;
 
 console.log('connecting to', url);
 
-mongoose.connect(url);
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB', error.message);
+    });
 
 const personSchema = new mongoose.Schema({
     name: String,
     number: String
 });
 
-const Person = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema);
 
-if (process.argv.length === 3) {
-    console.log('Phonebook:');
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person.name, person.number);
-        });
-        mongoose.connection.close();
-    });
-} else if (process.argv.length < 6) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-    });
+// if (process.argv.length === 3) {
+//     console.log('Phonebook:');
+//     Person.find({}).then(result => {
+//         result.forEach(person => {
+//             console.log(person.name, person.number);
+//         });
+//         mongoose.connection.close();
+//     });
+// } else if (process.argv.length < 6) {
+//     const person = new Person({
+//         name: process.argv[3],
+//         number: process.argv[4],
+//     });
 
-    person.save().then(result => {
-        console.log('person saved!');
-        mongoose.connection.close();
-    });
-}
+//     person.save().then(result => {
+//         console.log('person saved!');
+//         mongoose.connection.close();
+//     });
+// }
