81d12d948dbf62b1b749228230799293631cfd50
Eric Chin <ericychin94@gmail.com>
2022-01-12 15:51:28 -0800

    Removed old validator, added catch block, and added condition for validation error

--- index.js
+++ index.js
@@ -58,29 +58,31 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name is missing'
-        });
-    }
+    // if (body.name === undefined) {
+    //     return response.status(400).json({
+    //         error: 'name is missing'
+    //     });
+    // }
 
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number is missing'
-        });
-    }
+    // if (body.number === undefined) {
+    //     return response.status(400).json({
+    //         error: 'number is missing'
+    //     });
+    // }
 
     const newPerson = new Person({
         name: body.name,
         number: body.number
     });
 
-    newPerson.save().then(savedPerson => {
-        response.json(savedPerson);
-    })
+    newPerson.save()
+        .then(savedPerson => {
+            response.json(savedPerson.toJSON());
+        })
+        .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -103,6 +105,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);
